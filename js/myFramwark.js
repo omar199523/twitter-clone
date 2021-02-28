1cea34a8f07d650bf0a2bcd430aa58546827a85c
omar199523 <model199523@gmail.com>
2021-02-28 18:46:08 +0200

    this is last vargine

--- js/myFramwark.js
+++ js/myFramwark.js
@@ -1,14 +1,16 @@
 const createAndAddElement=(object,parantElement=undefined)=>{
     parantElement?parantElement:parantElement=undefined;
     //this is variable from object
-    let {element,innerElement,attribute={}}=object;
+    let {element,innerElement,attribute={},events={}}=object;
     // this is create element 
     let el = document.createElement(element);
     //this set attribute in el element
     for(const [atr,x] of Object.entries(attribute)){
         el.setAttribute(atr,x);
       }
-
+      for(const [event,fun] of Object.entries(events)){
+        el.addEventListener(event,fun);
+      }
     // this chack of inner Element if string plasce in el if not array set element from Array in el element
     if(Array.isArray(innerElement)){
         el.append(...innerElement);
